6f47e35c429e9ef184ede362fdb92c3751ea916f
pjohan747 <pjohan747@gmail.com>
2026-01-25 19:47:49 -0500

    Update main.py
    Varibles assignment

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+answer1 = 5 * 6
+print("The answer to your query is",answer1)
+
+answer2 = 8 / 3
+print("The answer to your query is",answer2)
+
+answer3 = 8 // 3
+print("The answer to your query is",answer3)
+
+answer4 = 8 % 3
+print("The answer to your query is",answer4)
+
+answer5 = 4200000 + 23090
+print("The answer to your query is",answer5)
+
+answer6 = 4200000 - 7633
+print("The answer to your query is",answer6)
+
 print("Johan Patel")
+
