0cfa704c7ec6e12ae21c0e3c77df521513ffc8a3
pjohan747 <pjohan747@gmail.com>
2026-02-03 16:45:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-answer1 = 5 * 6
-print("The answer to your query is",answer1)
+#That we want to multiply two numbers
+numMulti = int(input("Enter a number: "))
+numMuti2 = int(input("Enter another number: "))
+answerMulti = numMulti * numMuti2
+print("The answer to your query is", answerMulti)
 
-answer2 = 8 / 3
-print("The answer to your query is",answer2)
+#That we want to divide two numbers
+numDiv = int(input("Enter a number: "))
+numDiv2 = int(input("Enter another number: "))
+answerDiv = numDiv / numDiv2
+print("The answer to your query is", answerDiv)
 
-answer3 = 8 // 3
-print("The answer to your query is",answer3)
+#That we want to divide without remainder two numbers
+numNoRemain = int(input("Enter a number: "))
+numNoRemain2 = int(input("Enter another number: "))
+answerNoReamin = numNoRemain // numNoRemain2
+print("The answer to your query is", answerNoReamin)
 
-answer4 = 8 % 3
-print("The answer to your query is",answer4)
+#That we want the remainder two numbers
+numRemain = int(input("Enter a number: "))
+numRemain2 = int(input("Enter another number: "))
+answerReamin = numRemain % numRemain2
+print("The answer to your query is", answerReamin)
 
-answer5 = 4200000 + 23090
-print("The answer to your query is",answer5)
+#That we want the Sum two numbers
+numSum = int(input("Enter a number: "))
+numSum2 = int(input("Enter another number: "))
+answerSum = numSum + numSum2
+print("The answer to your query is", answerSum)
 
-answer6 = 4200000 - 7633
-print("The answer to your query is",answer6)
+#That we want the Subtraction two numbers
+numSub = int(input("Enter a number: "))
+numSub2 = int(input("Enter another number: "))
+answerSub = numSub - numSub2
+print("The answer to your query is", answerSub)
 
-print("Johan Patel")
+
+
+
 
